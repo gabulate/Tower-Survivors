93f1fc86656bded3b77a57430668d4cd94eb2201
Gabulate <gabrielulate71@gmail.com>
2024-03-24 22:43:15 -0600

    Fixed some collision issues

--- Assets/Code/Projectiles/Dart.cs
+++ Assets/Code/Projectiles/Dart.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+    using System.Collections;
 using System.Collections.Generic;
 using TowerSurvivors.Enemies;
 using UnityEngine;
@@ -7,27 +7,6 @@ namespace TowerSurvivors.Projectiles
 {
     public class Dart : BasicProjectile
     {
-        protected override void OnTriggerEnter2D(Collider2D other)
-        {
-            if (_enough)
-                return;
-
-            //When hitting an enemy, deals damage and subtract the passThrough property by 1.
-            if (_enemyLayer == (_enemyLayer | (1 << other.gameObject.layer)))
-            {
-                Enemy e = other.gameObject.GetComponent<Enemy>();
-                if (!e.isAlive)
-                    return;
-
-                e.TakeDamage(damage);
-                passThrough--;
-                //If it can't pass through anymore enemies, destroys itself.
-                if (passThrough <= -1)
-                {
-                    DestroyAnim();
-                }
-            }
-        }
         protected override void DestroyAnim()
         {
             _enough = true;
